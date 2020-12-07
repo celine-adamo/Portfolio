ef7f6d6e2fb5ba03e1d59a169361dff72c24f569
celine-adamo <71389471+celine-adamo@users.noreply.github.com>
2020-12-07 09:23:36 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,219 @@
-<?php require_once "header.php"
+<!DOCTYPE html>
+<html lang="fr">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adamo Céline</title>
+    <link rel="shortcut icon" href="assets/images/adamo.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <script src="https://kit.fontawesome.com/e80851bb12.js" crossorigin="anonymous" defer></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-dark navbar-default">
+            <div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a href="https://twitter.com/"><i class="fab fa-twitter text-light"></i></a>
+                <a href="https://facebook.com/"><i class="fab fa-facebook-f text-light"></i></a>
+                <a href="https://instagram.com/"><i class="fab fa-instagram text-light"></i></a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div>
+            <img src="assets/images/back5.png" class="w-100" alt="fond">
+        </div>
+    </header>
+
+    <section class="container">
+        <div class="flex-row row">
+            <div class="col-md-6">
+                <img src="assets/images/celine4.png" alt="celine" class="w-100">
+            </div>
+            <div class="col-md-5 offset-md-1">
+                <h1 style="text-align:justify; color:#FBB03B">Bienvenue.</h1>
+                <h5 style="text-align:justify; color:#F7931E">
+                    <br>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis corporis molestias doloribus animi
+                    quibusdam? Odio asperiores dolorem inventore labore enim ducimus earum, error nesciunt odit
+                    reiciendis officia autem maxime sit!
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis corporis molestias doloribus animi
+                    quibusdam? Odio asperiores dolorem inventore labore enim ducimus earum, error nesciunt odit
+                    reiciendis officia autem maxime sit!
+                </h5>
+            </div>
+        </div>
+    </section>
+    <br>
+    <h2 class="font-weight-bold my-5 text-center " style="color:#7CA3E6">Projets</h2>
+
+    <section class="container">
+        <div class="flex-row row">
+            <div class="col-md-12">
+                <div id="mdb-lightbox-ui"></div>
+                <div class="mdb-lightbox">
+
+                    <figure class="col-md-5 float-md-right">
+                        <a href="https://mdbootstrap.com/img/Mockups/Lightbox/Original/img%20(66).jpg" data-size="1600x1067">
+                            <img src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(66).jpg" class="img-fluid z-depth-1">
+                        </a>
+                    </figure>
+
+                    <figure class="col-md-5">
+                        <a href="https://mdbootstrap.com/img/Mockups/Lightbox/Original/img%20(65).jpg" data-size="1600x1067">
+                            <img src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(65).jpg" class="img-fluid z-depth-1">
+                        </a>
+                    </figure>
+
+                    <figure class="col-md-5 float-md-right">
+                        <a href="https://mdbootstrap.com/img/Mockups/Lightbox/Original/img%20(67).jpg" data-size="1600x1067">
+                            <img src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg" class="img-fluid z-depth-1">
+                        </a>
+                    </figure>
+
+                    <figure class="col-md-5">
+                        <a href="https://mdbootstrap.com/img/Mockups/Lightbox/Original/img%20(68).jpg" data-size="1600x1067">
+                            <img src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(68).jpg" class="img-fluid z-depth-1">
+                        </a>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container my-5 py-5 z-depth-1">
+        <section class="px-md-5 mx-md-5 dark-grey-text text-center text-lg-left">
+            <div class="row">
+                <div class="col-lg-6 mb-4 mb-lg-0 d-flex align-items-center justify-content-center">
+                    <img src="assets/images/celine55.png" class="img-fluid" alt="dessins">
+                </div>
+                <div class="col-lg-6 mb-4 mb-lg-0">
+                    <h3 class="font-weight-bold" style="color:#7CA3E6">Je suis un titre original</h3>
+                    <p class="font-weight-bold"  style="color:#3F5797">Je suis un sous-titre</p>
+                    <p class="text-muted"  style="text-align:justify; color:#87BEF6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.</p>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <section class="container">
+        <div class="row">
+            <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-interval="10000">
+                        <img src="assets/images/IMG_4447.JPG" class="d-block w-100" alt="test">
+                    </div>
+                    <div class="carousel-item" data-interval="2000">
+                        <img src="assets/images/IMG_4434.jpg" class="d-block w-100" alt="test2">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/images/IMG_4438.JPG" class="d-block w-100" alt="test3">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </section>
+    <br>
+
+
+    <section class="container col-5">
+        <div class="mb-4">
+            <h2 class="h1-responsive font-weight-bold text-center my-4" style="color:#7CA3E6">Contact</h2>
+            <div class="row">
+                <div class="col-md-8 mb-md-0 mb-5">
+                    <p id="contact-response"></p>
+                    <form id="contact-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="name" name="name" class="form-control">
+                                    <label for="name" style="color:#3F5797">Nom</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="email" name="email" class="form-control">
+                                    <label for="email" style="color:#3F5797">Email</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="subject" name="subject" class="form-control">
+                                    <label for="subject" style="color:#3F5797">Sujet</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="md-form">
+                                    <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                                    <label for="message" style="color:#3F5797">Message</label>
+                                </div>
+                            </div>
+                        </div>
+                        <!--                         <div class="text-center text-md-left">
+                        <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Envoyer</a>
+                    </div> -->
+                        <button class="btn btn-primary" style="background-color: #3F5797; border-color: #3F5797;">
+                            Envoyer
+                        </button>
+                    </form>
+
+                    <div class="status"></div>
+                </div>
+                <div class="col-md-3 text-center">
+                    <ul class="list-unstyled mb-0">
+                        <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                            <p>Belfort, 90000 France</p>
+                        </li>
+                        <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                            <p>06 67 42 47 66</p>
+                        </li>
+                        <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                            <p>test@gmail.com</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <footer class="page-footer font-small">
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href=""> AdamoCeline.com</a>
+        </div>
+    </footer>
 
 
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+</body>
 
-<?php require_once "footer.php"
-
-?>
-    
-
+</html>
